397d0a54e532232b184cad17c1faefc444d944e7
xXm4ntr4Xx <pietroannobil@gmail.com>
2021-11-18 13:08:54 +0000

    new commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const myCat = {
+    color: "black",
+    name: "Tony",
+    lovesCuddles: true,
+};
+
+const house = {
+    stories: 2,
+    isParking:false,
+    bedrooms:2,
+    isGarden:true,
+    energyRating: "D"
+}
+
+const car ={
+    color: "red",
+    wheels: 4,
+    powerSteering: true,
+    seats: 2,
+    fuelEfficiency: "20 miles per gallon"
+}
+
+const movie = {
+    title: "The Dark Knigth",
+    released: "2008",
+    leadActor: "Christian Bale"
+}
---
+++ main3.js
@@ -0,0 +1,10 @@
+let desiredPleasantry = "greeting";
+
+let bensPhrases = {
+  greeting: "Well hello there!",
+  farewell: "See you in another life brother",
+  smallTalk: "Warm today isn't it...",
+};
+
+desiredPleasantry= "Welcome"
+console.log(`${bensPhrases.greeting} ${desiredPleasantry}`)
