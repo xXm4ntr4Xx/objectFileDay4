7d82fb803f691fd8a1914bebdfcf7234946f3704
xXm4ntr4Xx <pietroannobil@gmail.com>
2021-11-18 13:19:27 +0000

    everything

---
+++ main2.js
@@ -0,0 +1,10 @@
+let person = {
+    name: "Ben",
+    likesChocolate: true,
+  };
+
+  if(person.likesChocolate === true){
+      console.log("Ben loves chocolate")
+  }else{
+      console.log("Ben hates chocolate")
+  }
---
+++ main4.js
@@ -0,0 +1,7 @@
+let person = {
+    firstName: "firstName",
+    lastName: "lastName",
+    isBootcamper: false,
+  };
+  
+  person.isBootcamper = true;
